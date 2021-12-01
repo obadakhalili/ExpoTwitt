173354ffa6f33c73216e871a0b3f05ba5c0cc631
Obada Khalili <obada.akhalili@gmail.com>
2021-12-01 11:22:44 +0200

    setup naiveui and vfonts

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { defineComponent } from "vue"
+import { NH1 } from "naive-ui"
 
 export default defineComponent({
   name: "App",
-  render: () => <h1>Hello World</h1>,
+  render: () => <NH1>Hello World</NH1>,
 })
--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue"
+import "vfonts/Lato.css"
+
 import App from "./App.jsx"
 
 createApp(App).mount("#app")
