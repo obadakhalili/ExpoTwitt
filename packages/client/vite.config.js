1fd94a536d79376cca49b38c0c17a9853f755c87
Obada Khalili <obada.akhalili@gmail.com>
2021-12-03 09:07:57 +0200

    client: draw interest bounding box that's retrived from api on map

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -4,4 +4,14 @@ import vueJsx from "@vitejs/plugin-vue-jsx"
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
